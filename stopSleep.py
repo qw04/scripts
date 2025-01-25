8c107010e97e86de8d28c54c4326d7290be8bae7
qw04 <shahsaumya2004@gmail.com>
2025-01-25 23:58:00 +0000

    idek just the change that was present locally

--- stopSleep.py
+++ stopSleep.py
@@ -1,9 +1,33 @@
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Controller, HotKey, Listener
 import time
 import random
 import sys
+import threading
 
-keyboard = Controller()
+def thing(timeToSleep):
+	# does the sleep avoiding
+	controller = Controller()
+	time.sleep(2)
+	
+	while True:
+		controller.press(Key.alt)
+		controller.press(Key.tab)
+		controller.release(Key.alt)
+		controller.release(Key.tab)
+
+		time.sleep(0.1)
+
+		controller.press(Key.alt)
+		controller.press(Key.tab)
+		controller.release(Key.alt)
+		controller.release(Key.tab)
+		
+		time.sleep(timeToSleep)
+
+def on_activate():
+	print("hotkey pressed")
+	global thread
+	thread.join()
 
 
 def main():
@@ -11,22 +35,16 @@ def main():
 		raise Exception("Usage python stopSleep <time to sleep between 2 alt-tabs>")
 
 	else:	
-		timeToSleep = float(sys.argv[1])
-		time.sleep(2)
-		while True:
-			keyboard.press(Key.alt)
-			keyboard.press(Key.tab)
-			keyboard.release(Key.alt)
-			keyboard.release(Key.tab)
+		start = time.time()
+		global thread
+		thread = threading.Thread(target=thing, args=(float(sys.argv[1]),))
+		thread.start()
 
-			time.sleep(0.1)
-
-			keyboard.press(Key.alt)
-			keyboard.press(Key.tab)
-			keyboard.release(Key.alt)
-			keyboard.release(Key.tab)
-			
-			time.sleep(timeToSleep)
+		hotkey = HotKey(HotKey.parse('<ctrl>+<alt>+h'), on_activate = on_activate)
+		with Listener(on_press=lambda k: hotkey.press(l.canonical(k))) as l:
+			l.join()
+		
+		print(f"program ran for{time.time() - start} seconds")
 
 
 if __name__ == "__main__":
